709fedd0daf7ceb05b3bc0b49b2ac0df7a9ad270
Darshil Papalkar <67659174+Darshil-Papalkar@users.noreply.github.com>
2020-09-04 10:17:28 +0530

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -2,16 +2,20 @@ import smtplib
 from email.message import EmailMessage
 
 email = EmailMessage()
+# Sender's name
 email['from'] = 'Darshil Papalkar'
-email['to'] = 'papalkardarshil13@gmail.com'
-email['subject'] = 'You are genius'
-
-email.set_content('I am a Python Master')
+# Receiver's mail id
+email['to'] = 'receiver@gmail.com'
+#  Email Subject
+email['subject'] = 'SUBJECT'
+# Email Content
+email.set_content('CONTENT')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    file = open('C:\\Users\\DARSHIL\\OneDrive_1\\OneDrive\\Desktop\\python-Voice Recognizer\\JARVIS\\pwd.txt', 'r')
-    smtp.login('papalkardarshil12@gmail.com', file.readline())
+#     Create a text document and store password
+    file = open('text\\document\\pwd.txt', 'r')
+    smtp.login('sender@gmail.com', file.readline())
     smtp.send_message(email)
-    print("All Done!")
+print("All Done!")
